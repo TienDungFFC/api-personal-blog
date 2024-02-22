cb0f8438ace17bc2d51adfb01f20fdf09374eb29
Nguyen Tien Dung <tiendungntd99@gmail.com>
2024-02-22 17:25:51 +0700

    add id to post response

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -15,6 +15,7 @@ class PostResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'slug' => $this->slug,
             'description' => $this->description,
